8168f661592662aa8105894c11f3e4d770f88430
ericmartinezr <eric.martinez@live.cl>
2026-05-10 21:00:57 -0400

    feat: adds shell to run dataflow without building

---
+++ run_dataflow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PROJECT="etl-censo-d"
+REGION="us-central1"
+BUCKET="gs://etl-censo-df"
+BQ_DATASET="ds_censo"
+BQ_TABLE="tbl_censo"
+
+python -m gcp \
+    --runner DataflowRunner \
+    --project $PROJECT \
+    --region $REGION \
+    --temp_location $BUCKET/temp \
+    --staging_location $BUCKET/staging \
+    --sdk_location container \
+    --save_main_session True \
+    --enable_hot_key_logging \
+    --dataset $BQ_DATASET \
+    --table $BQ_TABLE \
+    --input_location $BUCKET/input \
+    --output_location $BUCKET/out
